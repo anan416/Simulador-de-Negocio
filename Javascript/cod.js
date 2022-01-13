93a09ff862b9eb3ae29b40fa3a3c0e801b9b092e
anan416 <90143779+anan416@users.noreply.github.com>
2022-01-13 15:26:12 -0300

    Add files via upload

--- Javascript/cod.js
+++ Javascript/cod.js
@@ -3,10 +3,6 @@ var imagenes = ["Imagenes/fondo1.png", "Imagenes/fondo2.png", "Imagenes/fondo3.p
 var imagen = document.querySelector(".imgCarrousel");
 imagen.setAttribute("src", imagenes[0]);
 
-function enlace(){
-    alert("El link aún no se encuentra disponible.");
-};
-
 function imagen1(){
     imagen.setAttribute("src", imagenes[0]);
 }
@@ -18,3 +14,16 @@ function imagen2(){
 function imagen3(){
     imagen.setAttribute("src", imagenes[2]);
 }
+
+function mostrar(){
+    if (document.querySelector(".Resnavegador").style.display == "flex"){
+        document.querySelector(".Resnavegador").style.display = "none";
+    }
+    else{
+        document.querySelector(".Resnavegador").style.display = "flex";
+    }
+}
+
+function enlace(){
+    alert("El link aún no se encuentra disponible.");
+};
