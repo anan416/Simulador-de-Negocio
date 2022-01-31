7f3745e5d0c32f1e5645f99f36ae2bcd6bc054de
anan416 <90143779+anan416@users.noreply.github.com>
2022-01-31 11:17:16 -0300

    Add files via upload

--- Javascript/cod.js
+++ Javascript/cod.js
@@ -1,19 +1,3 @@
-var imagenes = ["Imagenes/fondo1.png", "Imagenes/fondo2.png", "Imagenes/fondo3.png"];
-
-var imagen = document.querySelector(".imgCarrousel");
-imagen.setAttribute("src", imagenes[0]);
-
-function imagen1(){
-    imagen.setAttribute("src", imagenes[0]);
-}
-
-function imagen2(){
-    imagen.setAttribute("src", imagenes[1]);
-}
-
-function imagen3(){
-    imagen.setAttribute("src", imagenes[2]);
-}
 
 function mostrar(){
     if (document.querySelector(".Resnavegador").style.display == "flex"){
